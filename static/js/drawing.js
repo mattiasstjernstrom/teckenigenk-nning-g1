52f588496dab42afc40184c9e379d7a00cdc4543
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-05-10 15:41:41 +0200

    Some misstakes in js fixed

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -26,6 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const fileInput = document.getElementById("file");
+    fileInput.value = "";
   }
 
   canvas.addEventListener("mousedown", startPosition);
@@ -49,35 +51,14 @@ function drawImageOnCanvas(input) {
     reader.onload = function (e) {
       const img = new Image();
 
-      // När bilden har laddats, rita den på canvas
       img.onload = function () {
         const canvas = document.getElementById("drawingCanvas");
         const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Rensa canvas om det behövs
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height); // Anpassa storleken efter behov
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
       };
 
-      img.src = e.target.result; // Triggar img.onload
-    };
-
-    reader.readAsDataURL(file); // Läs in filen som en Data URL
-  }
-}
-
-function convertToDataURL() {
-  const fileInput = document.getElementById("file");
-  const file = fileInput.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      const dataURL = e.target.result;
-      console.log(dataURL); // Du kan nu använda denna Data URL för att skicka till servern eller bearbeta vidare
-
-      const img = document.createElement("img");
-      img.src = dataURL;
-      document.body.appendChild(img);
+      img.src = e.target.result;
     };
 
     reader.readAsDataURL(file);
