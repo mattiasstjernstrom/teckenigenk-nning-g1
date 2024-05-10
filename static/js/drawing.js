cdcbbd76d15b1478e4c87703723818558812414e
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-05-10 21:05:39 +0200

    Fixed some javascript

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -1,8 +1,9 @@
+let hasDrawn = false;
 document.addEventListener("DOMContentLoaded", function () {
   const canvas = document.getElementById("drawingCanvas");
   const ctx = canvas.getContext("2d");
   let painting = false;
-
+  
   function startPosition(e) {
     painting = true;
     draw(e);
@@ -11,13 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
   function finishedPosition() {
     painting = false;
     ctx.beginPath();
+    if (!hasDrawn) hasDrawn = true;  // Update hasDrawn when the user has drawn
   }
 
   function draw(e) {
     if (!painting) return;
     ctx.lineWidth = 5;
     ctx.lineCap = "round";
-
     ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     ctx.stroke();
     ctx.beginPath();
@@ -26,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    hasDrawn = false;  // Reset hasDrawn when canvas is cleared
     const fileInput = document.getElementById("file");
     const label = document.querySelector("label[for=file]");
     fileInput.value = "";
@@ -37,33 +39,31 @@ document.addEventListener("DOMContentLoaded", function () {
   canvas.addEventListener("mousemove", draw);
 
   window.clearCanvas = clearCanvas;
+
+  const label = document.querySelector("label[for=file]");
+  label.addEventListener("click", function(event) {
+    if (hasDrawn && !confirm("Are you sure you want to replace the canvas? This will clear the current drawing.")) {
+      event.preventDefault();  // Prevent the file dialog from continuing
+      label.value = "";    // Reset the file input
+    } else {
+      loadImage(event.target);
+    }
+  });
 });
 
-function drawImageOnCanvas(input) {
+function loadImage(input) {
   const canvas = document.getElementById("drawingCanvas");
   const ctx = canvas.getContext("2d");
-  const dataURL = canvas.toDataURL();
-  if (dataURL !== "data:,") {
-    const confirmation = confirm(
-      "Are you sure you want to replace the canvas? This will clear the current drawing."
-    );
-    if (!confirmation) {
-      return;
-    }
-  }
   if (input.files && input.files[0]) {
     const file = input.files[0];
     const reader = new FileReader();
-    const fileName = file.name;
     const label = document.querySelector("label[for=file]");
-    label.innerText = fileName ?? "Browse Files";
+    label.innerHTML = "Uploaded file: <strong class='whitebg'>" + file.name + "</strong>" ?? "Browse Files";
 
     reader.onload = function (e) {
       const img = new Image();
 
       img.onload = function () {
-        const canvas = document.getElementById("drawingCanvas");
-        const ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
       };
