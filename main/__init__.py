df4484dd0e1e122c999fa1fe3c351d7d45630418
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-05-10 09:38:06 +0200

    Added some trees

--- main/__init__.py
+++ main/__init__.py
@@ -4,6 +4,13 @@ from flask import Blueprint
 main = Blueprint("main", __name__)
 
 
+# Main site route:
 @main.route("/")
 def home():
     return "Drawing area"
+
+
+# Api route, to check the drawing area:
+@main.route("/api")
+def api():
+    return "Api route"
