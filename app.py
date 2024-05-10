cf810b73dd994fe175d393fa63e4b0f247b9eac5
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-05-10 09:09:33 +0200

    Initializing a new repository

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Blueprint for main routes:
+from main import main as main_routes
+
+app.register_blueprint(main_routes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port="5000")
